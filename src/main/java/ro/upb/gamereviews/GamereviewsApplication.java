2bb6fde33c47b5682054e8c52035d4300d0e6ed6
adrian <adrian.sandu113@gmail.com>
2023-11-14 20:11:56 +0200

    second commit

---
+++ src/main/java/ro/upb/gamereviews/GamereviewsApplication.java
@@ -0,0 +1,13 @@
+package ro.upb.gamereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamereviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamereviewsApplication.class, args);
+	}
+
+}
